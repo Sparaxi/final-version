297a1662252603534057bc337762e8d6757caa33
Dayton van Wilsem <90177344+Daytonvw@users.noreply.github.com>
2021-12-15 22:36:36 +0100

    Update main.ts, tilemap.g.jres, tilemap.g.ts, images.g.jres, images.g.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,21 @@
+function grasslands() {
+    let Hero: Sprite = null
+    tiles.setTilemap(tilemap`grasslands_level`)
+    Hero = sprites.create(assets.image`hero walking down`, SpriteKind.Player)
+    controller.moveSprite(Hero)
+    scene.cameraFollowSprite(Hero)
+    function heroMovement() {
 
+        controller.moveSprite(Hero)
+        controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
+            Hero.setImage(assets.image`hero walking up`)
+        })
+        controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
+            Hero.setImage(assets.image`hero walking right`)
+        })
+        controller.down.onEvent(ControllerButtonEvent.Pressed, function () {
+            Hero.setImage(assets.image`hero walking down`)
+        })
+        controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
+            Hero.setImage(assets.image`hero walking left`)
+        })
